680b8a95ca05507888030dbf7513f137d6cd2f55
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-05-28 17:27:38 -0700

    Deployed to Rinkeby

---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["yahaha!", "guy", "sticks"],
+    [
+      "https://i.imgur.com/SLJGBcM.png",
+      "https://i.imgur.com/vta4Xm3.png",
+      "https://i.imgur.com/xviyD0A.png",
+    ],
+    [100, 200, 300],
+    [100, 50, 25]
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #2");
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log("Minted NFT #3");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #4");
+
+  console.log("Done deploying and minting!");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
