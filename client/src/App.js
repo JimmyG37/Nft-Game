9d3f5b6072081e746d36b78cf218b81b78b33ed9
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-05-28 23:18:45 -0700

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Stop Global Warming ⚔️</p>
+          <p className="sub-text">
+            Team up to protect the Global from Warming!
+          </p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              alt="Monty Python Gif"
+            />
+          </div>
+        </div>
+        <div className="footer-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
